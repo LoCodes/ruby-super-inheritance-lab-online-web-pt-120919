05a8a45d19fdc051bdd521dad3107141cd96510c
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-05 00:08:12 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,16 +1,16 @@
 class ChattyStudent < Student
 
-  def hello
+  def hello   # #hello returns a greeting phrase plus a long chatty phrase
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
 
-  def raise_hand
-    10.times do
+  def raise_hand  # #raise_hand the phrase 'Pick me!' ten times
+    10.times do   # No "puts" because
     super
 
-#    super
+#    super       #You can literally
 #    super
 #    super
 #    super
